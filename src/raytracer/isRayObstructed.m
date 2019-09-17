4e443d5d1ccb30dc95b5c9471e631586bd041592
Mattia Lecci <mattia.lecci@gmail.com>
2019-09-17 12:23:01 +0200

    Bugfix: isRayObstructed
    True even if ray just touches a plane. Corrected this anomalous behavior. Added 1e-9 tolerance.

--- src/raytracer/isRayObstructed.m
+++ src/raytracer/isRayObstructed.m
@@ -7,14 +7,13 @@ for i = 1:size(cadData,1)
         continue
     end
     
-    triang = cadData(i,:);
-    planeEq = triang(10:13);
-    vertex1 = triang(1:3);
-    vertex2 = triang(4:6);
-    vertex3 = triang(7:9);
+    planeEq = cadData(i,10:13);
+    vertex1 = cadData(i,1:3);
+    vertex2 = cadData(i,4:6);
+    vertex3 = cadData(i,7:9);
     
-    intersection = planeIntersectSegment(planeEq, p1, p2);
-    if ~isempty(intersection) &&...
+    [intersection,t] = planeIntersectSegment(planeEq, p1, p2);
+    if t > 1e-9 && t < 1-1e-9 &&... % intersect, not just "touch"
             pointInTriangle(intersection, vertex1, vertex2, vertex3)
         isObstructed = true;
         return
