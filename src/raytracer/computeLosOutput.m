c832e7112bbe8e3606db0871bf89fded59925b37
Mattia Lecci <mattia.lecci@gmail.com>
2019-09-17 13:57:13 +0200

    Add computeLosOutput
    New function to check existence and compute output for LoS ray

---
+++ src/raytracer/computeLosOutput.m
@@ -0,0 +1,21 @@
+function [output, multipath] = computeLosOutput(rxPos, txPos, rxVel, txVel,...
+    cadData, freq)
+
+isObstructed = isRayObstructed(rxPos, txPos, cadData, []);
+if isObstructed
+   output = [];
+   multipath = [];
+   
+else
+    reflOrder = 0;
+    dod = rxPos - txPos;
+    doa = -dod;
+    rayLen = norm(dod);
+    pathGain = friisPathGain(rayLen, freq);
+    dopplerFactor = getDopplerFactor(txPos, rxPos, txVel, rxVel, [], []);
+    
+    output = fillOutput(reflOrder, dod, doa, rayLen, pathGain, dopplerFactor, freq);
+    multipath = [rxPos, txPos];
+end
+
+end
