92e523cf73112c1ffe221d827ccd0eea46967829
Mattia Lecci <mattia.lecci@gmail.com>
2019-09-17 14:36:50 +0200

    Update mymultipath to follow the new functions

--- src/raytracer/mymultipath.m
+++ src/raytracer/mymultipath.m
@@ -1,40 +1,31 @@
-function output = mymultipath(rxPos, txPos, rxVel, txVel, triangIdxList,...
-    cadData, MaterialLibrary, switchQd, switchMaterial, freq)
+function [output, multipath] = mymultipath(rxPos, txPos, rxVel, txVel, triangIdxList,...
+    cadData, materialLibrary, switchQd, switchMaterial, freq)
 
 triangListLen = size(triangIdxList,1);
 reflOrder = size(triangIdxList,2);
 
 output = nan(triangListLen, 21);
-
 multipath = nan(triangListLen, 1 + 3 * (reflOrder+2));
+
 multipath(:,1) = reflOrder;
-multipath(:,2:4) = repmat(rxPos,triangListLen,1); % TODO: maybe not necessary
 
 for i = 1:triangListLen
     currentTriangIdxs = triangIdxList(i,:);
-    % Extract variables for singleMultipathGenerator
-    arrayOfPlanes = getArrayOfPlanes(currentTriangIdxs,cadData);
-    arrayOfPoints = getArrayOfPoints(currentTriangIdxs,cadData);
     
-    reflected = rxPos; % To start recursion. Rx is reflected, not Tx
+    [pathExists, dod, doa, rowMultipath, rayLen, dopplerFactor, pathGain]...
+    = computeSingleRay(txPos, rxPos, txVel, rxVel, currentTriangIdxs,...
+    cadData, materialLibrary, switchQd, switchMaterial, freq);
     
-    [isValidPath, ~, dod, doa, multipath, rayLen, dopplerFactor,...
-    extraPathLoss, ~, ~, ~, ~, ~] = singleMultipathGenerator(...
-        1, reflOrder, 1, arrayOfPlanes, arrayOfPoints,...
-        reflected, rxPos, txPos, cadData, multipath, i, txVel, rxVel,...
-        [], [], [], [], [], [], []);
-    
-    computeSingleRay(txPos, rxPos, txVel, rxVel, triangIdxList(i,:),...
-    cadData, MaterialLibrary, switchQd, switchMaterial, freq);
-    
-    if isValidPath == 1
+    if pathExists
         output(i,:) = fillOutput(reflOrder, dod, doa, rayLen,...
-            -extraPathLoss, dopplerFactor, freq);
+            pathGain - 10, dopplerFactor, freq);
+        multipath(i,2:end) = rowMultipath;
     end
 end
 
 % Remove invalid output rows
 invalidRows = all(isnan(output), 2);
 output(invalidRows,:) = [];
+multipath(invalidRows,:) = [];
 
 end
