02a0c174fddb9fbb491a9c4173b2050b633202a3
Paolo Testolina <p.testolina@gmail.com>
2019-10-17 20:21:03 +0200

    Loading scenario option and stats collection
    With the load flag, it is possible to decide whether to load the scenario from the currFolder path, or to generate it from scratch. 
    Stats for different metrics are computed and stored in the stats struct.

--- src/collectStats.m
+++ src/collectStats.m
@@ -19,55 +19,102 @@ clc
 
 addpath('raytracer', 'utils')
 
-% specify node ID to select file to be loaded
+% specify node ID to select file to be loaded (default: txId=0, rxId=1)
 txId = 0;
 rxId = 1;
 
 scenario = 'L-Room';
 scenarioPath = fullfile('statsScenarios',scenario);
 
-% save every execution in a different folder
-currFolder = fullfile(scenarioPath,datestr(now,'mm-dd-yyyy HH-MM-SS'));
+load = true;
+if load
+    loadFolder = "10-17-2019 17-26-22";
+    currFolder = fullfile(scenarioPath,loadFolder);
+    sprintf("Loading files from %s.",currFolder);
+else
+    % save every execution in a different folder
+    currFolder = fullfile(scenarioPath,datestr(now,'mm-dd-yyyy HH-MM-SS'));
+    print("Creating new scenario in %s",currFolder);
+    
+    %% config parameters
+    inputPath = fullfile(currFolder,'Input');
+    mkdir(inputPath)
+    cfgParams.numberOfTimeDivisions = 200; % specify the number of different positions for the stats
+    genParameterCfg(cfgParams,inputPath);
 
-%% config parameters
-inputPath = fullfile(currFolder,'Input');
-mkdir(inputPath)
-cfgParams.numberOfTimeDivisions = 200;
-genParameterCfg(cfgParams,inputPath);
+    %% create TX-RX grid
+    % specify the TX position
+    txPos = [0.2,3,2.5]; % the transmitter position is fixed
+    visualize=true; % plot scatterer positions
+    randomSampling = true; % either random or grid positions for collecting the stats
+    createTxRxGrid(currFolder,randomSampling,txPos,visualize);
 
-%% create TX-RX grid
-% specify the TX position
-txPos = [0.2,3,2.5]; % the transmitter position is fixed
-visualize=true;
-randomSampling = true;
-createTxRxGrid(currFolder,randomSampling,txPos,visualize);
-
-%% execute the ray-tracer
-generateRayTracerOutFiles(currFolder)
-
+    %% execute the ray-tracer
+    generateRayTracerOutFiles(currFolder)
+end
 %% extract statistics
 outputPath = fullfile(currFolder,'Output','Ns3','QdFiles',sprintf('Tx%dRx%d.txt',txId,rxId));
 output = readQdFile(outputPath);
 
-feature = 'pathGain'; % specify the metric to observe for the statistics
+stats = struct();
+% path gain
+feature = "pathGain";
 if ~isfield(output,feature)
-    error('feature must be a field of Tx%sRx%s.txt',txId,rxId);
+    error('%s must be a field of Tx%sRx%s.txt',feature,txId,rxId);
 end
+pathGain = extractfield(output,feature);
+stats.(feature) = pathGain;
 
-currFeature = extractfield(output,feature);
+% delta path gain
+feature = "deltaPathGain";
+maxPathGain = max(pathGain);
+deltaPathGain = pathGain-maxPathGain;
+stats.(feature) = deltaPathGain;
 
-figure()
-ecdf(currFeature)
-xlabel('$x=$ Path Gain')
-ylabel('$F(x)$')
-% title('Path Gain distribution in a rectangular room. $N_{refl}=2$')
- 
-figure()
-histogram(currFeature)
-xlabel('Path Gain')
-ylabel('Count')
-% title('Path Gain distribution in a rectangular room. $N_{refl}=2$')
+% delay
+feature = "delay";
+if ~isfield(output,feature)
+    error('%s must be a field of Tx%sRx%s.txt',feature,txId,rxId);
+end
+delay = extractfield(output,feature);
+stats.(feature) = delay;
 
+% delay spread
+feature = "delaySpread";
+delaySpread = std(delay); % TODO: weigthed std w.r.t pathGain
+stats.(feature) = delaySpread;
+
+% angle spread
+feature = "aodEl";
+if ~isfield(output,feature)
+    error('%s must be a field of Tx%sRx%s.txt',feature,txId,rxId);
+end
+aodEl = extractfield(output,feature);
+stats.(feature) = aodEl;
+
+
+%% plot statistics
+features = ["pathGain"]; % specify the metric to observe for the statistics
+
+for feat = features
+    if ~isfield(output,feat)
+        error('feature must be a field of Tx%sRx%s.txt',txId,rxId);
+    end
+    currFeature = extractfield(output,feature);
+    
+    figure()
+    subplot(1,2,1)
+    ecdf(currFeature)
+    xlabel('$x=$ Path Gain')
+    ylabel('$F(x)$')
+    % title('Path Gain distribution in a rectangular room. $N_{refl}=2$')
+
+    subplot(1,2,2)
+    histogram(currFeature)
+    xlabel('Path Gain')
+    ylabel('Count')
+    % title('Path Gain distribution in a rectangular room. $N_{refl}=2$')
+end
 
 function []=generateRayTracerOutFiles(currPath)
     %% Initialization 
