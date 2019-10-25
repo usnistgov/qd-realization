89a882121d8d57213a53d21ac06b17b92541d709
Paolo Testolina <p.testolina@gmail.com>
2019-10-25 12:48:08 +0200

    Finalizing collectStats reorganization
    Plots will be handled in a separate script.
    collectStats saves the stats.mat files in a new /Stats directory that is on the same level as /Input and /Output.
    The stats are extracted either
    1. starting from existing output files (can be measurements or ray tracer output)
    2. starting from 
        2a. a new set of RX/TX positions (grid or random)
        2b. a given set of RX/TX positions, specified in the /Input folder

--- src/collectStats.m
+++ src/collectStats.m
@@ -23,39 +23,59 @@ addpath('raytracer', 'utils', 'utils/statsUtils')
 txId = 0;
 rxId = 1;
 
-scenario = 'L-Room';
-scenarioPath = fullfile('statsScenarios',scenario);
+scenario = 'Camillo_NIST_60-GHz Lecture Room--MPCs_1551899818';
+% scenario = 'Indoor1';
+scenarioPath = fullfile('realScenarios',scenario);
+% scenarioPath = fullfile('statsScenarios',scenario);
 
-load = true;
-if load
-    loadFolder = "10-18-2019 14-06-25";
-    currFolder = fullfile(scenarioPath,loadFolder);
-    sprintf("Loading files from %s.",currFolder);
-else
-    % save every execution in a different folder
-    currFolder = fullfile(scenarioPath,datestr(now,'mm-dd-yyyy HH-MM-SS'));
-    sprintf("Creating new scenario in %s",currFolder);
+%% load output file (measurements or ray tracer output)
+loadExternalOutput = true;
+if loadExternalOutput
+    % specify output path 
+    dataFolder = "data";
+    fileName = 'output.mat';
+    currFolder = fullfile(scenarioPath,dataFolder);
+    outputPath = fullfile(currFolder,'Output',fileName);
     
-    %% config parameters
-    inputPath = fullfile(currFolder,'Input');
-    mkdir(inputPath)
-    cfgParams.numberOfTimeDivisions = 200; % specify the number of different positions for the stats
-    genParameterCfg(cfgParams,inputPath);
+    load(outputPath); % load output
 
-    %% create TX-RX grid
-    % specify the TX position
-    txPos = [0.2,3,2.5]; % the transmitter position is fixed
-    visualize=true; % plot scatterer positions
-    randomSampling = true; % either random or grid positions for collecting the stats
-    createTxRxGrid(currFolder,randomSampling,txPos,visualize);
-
+else
+    %% TX-RX grid
+    createNewRxGrid = true;
+    if createNewRxGrid
+        % create a new TX-RX grid
+        % save every execution in a different folder
+        currFolder = fullfile(scenarioPath,datestr(now,'mm-dd-yyyy HH-MM-SS'));
+        sprintf("Creating new scenario in %s",currFolder);
+        
+        %% config parameters
+        inputPath = fullfile(currFolder,'Input');
+        mkdir(inputPath)
+        
+        cfgParams.numberOfTimeDivisions = 200; % specify the number of different positions for the stats
+        genParameterCfg(cfgParams,inputPath);
+        
+        % create new TX-RX grid
+        txPos = [0.2,3,2.5]; % specify the TX position (fixed)
+        visualize=true; % plot scatterer positions
+        randomSampling = true; % either random or grid positions for collecting the stats
+        createTxRxGrid(currFolder,randomSampling,txPos,visualize);
+    
+    else % setup inputs based on existing files
+        currFolder = fullfile(scenarioPath,'10-25-2019 12-12-49');
+        inputPath = fullfile(currFolder,'Input');
+        
+        cfgParams.numberOfTimeDivisions = 108; % specify the number of different positions for the stats
+        genParameterCfg(cfgParams,inputPath);
+    end
     %% execute the ray-tracer
-    generateRayTracerOutFiles(currFolder)
+    outputPath = generateRayTracerOutFiles(currFolder);
+    qdPath = fullfile(outputPath,"Ns3","QdFiles",sprintf("Tx%dRx%d.txt",txId,rxId));
+    output = readQdFile(qdPath);
+    save(fullfile(outputPath,'output.mat'),'output')
 end
-%% extract statistics
-outputPath = fullfile(currFolder,'Output','Ns3','QdFiles',sprintf('Tx%dRx%d.txt',txId,rxId));
-output = readQdFile(outputPath);
 
+% check output file fields
 if ~isfield(output,"pathGain") || ~isfield(output,"delay") || ~isfield(output,"aodAz") || ~isfield(output,"aodEl")...
         || ~isfield(output,"aodAz") || ~isfield(output,"aoaEl")
     error('Missing field in Tx%sRx%s.txt',txId,rxId);
@@ -63,80 +83,39 @@ end
 
 %% extract statistics
 statsFilePath = extractStats(output,currFolder);
-load(statsFilePath)
+sprintf("Statistics saved in %s",statsFilePath);
 
-%% plot statistics
-% select plot metrics
-features = ["pathGain","delay","aoaAz"]; % specify the metric to observe for the statistics
-if strcmp(features,"all") % if "all" all the stats will be plotted
-    features = fieldnames(stats);
+function outputPath=generateRayTracerOutFiles(currPath)
+%% Initialization
+rootFolderPath = pwd;
+fprintf('-------- NIST/CTL QD mmWave Channel Model --------\n');
+fprintf('Current root folder:\n\t%s\n',rootFolderPath);
+[path,folderName] = fileparts(rootFolderPath);
+if strcmp(folderName, 'src')
+    fprintf('Start to run.\n');
+else
+    error('The root folder should be ''src''');
 end
 
-% whether to save the figures or not
-savefigs = 1; 
-if savefigs 
-    endout=regexp(statsFilePath,filesep,'split');
-    statsFigFolder = endout{1:end-1}; % path where to save the plots
-end
+%% Input
+endout=regexp(currPath,filesep,'split');
+scenarioNameStr = endout{2};
+sprintf('Use scenario: %s.\n',scenarioNameStr);
 
-for featID = 1:length(features)
-    feat = features{featID};
-    
-    if ~isfield(stats,feat)
-        error('feature must be a field of Tx%sRx%s.txt',txId,rxId);
-    end
-    currFeature = extractfield(stats,feat);
-    
-    figure(featID)
-    subplot(1,2,1)
-    ecdf(currFeature)
-    xlabel(sprintf('$x=$ %s',feat))
-    ylabel('$F(x)$')
-    % title('Path Gain distribution in a rectangular room. $N_{refl}=2$')
-    hold on
-    
-    subplot(1,2,2)
-    histogram(currFeature,'Normalization','pdf')
-    xlabel(feat)
-    ylabel('Count')
-    % title('Path Gain distribution in a rectangular room. $N_{refl}=2$')
-    hold on
-    
-    fileName = fullfile(statsFigFolder,sprintf('%s.fig',feat));
-    savefig(fileName)
-end
+% Check Input Scenario File
+scenarioInputPath = fullfile(currPath,'/Input');
 
-function []=generateRayTracerOutFiles(currPath)
-    %% Initialization 
-    rootFolderPath = pwd;
-    fprintf('-------- NIST/CTL QD mmWave Channel Model --------\n');
-    fprintf('Current root folder:\n\t%s\n',rootFolderPath);
-    [path,folderName] = fileparts(rootFolderPath);
-    if strcmp(folderName, 'src')
-        fprintf('Start to run.\n');
-    else
-        error('The root folder should be ''src''');
-    end
+% Input System Parameters
+paraCfg = parameterCfg(currPath);
+% Input Node related parameters
+[paraCfg, nodeCfg] = nodeProfileCfg(paraCfg);
 
-    %% Input
-    endout=regexp(currPath,filesep,'split');
-    scenarioNameStr = endout{2};
-    sprintf('Use scenario: %s.\n',scenarioNameStr);
+% To collect the statistics, we need these two switches
+paraCfg.switchSaveVisualizerFiles = 1;
+paraCfg.mobilitySwitch = 1;
+% Run raytracing function and generate outputs
+outputPath = Raytracer_collectStats(paraCfg, nodeCfg);
 
-    % Check Input Scenario File
-    scenarioInputPath = fullfile(currPath,'/Input');
-
-    % Input System Parameters
-    paraCfg = parameterCfg(currPath);
-    % Input Node related parameters
-    [paraCfg, nodeCfg] = nodeProfileCfg(paraCfg);
-
-    % To collect the statistics, we need these two switches
-    paraCfg.switchSaveVisualizerFiles = 1;
-    paraCfg.mobilitySwitch = 1;
-    % Run raytracing function and generate outputs
-    outputPath = Raytracer_collectStats(paraCfg, nodeCfg);
-
-    fprintf('Save output data to:\n%s\n',outputPath);
-    fprintf('--------- Simulation Complete ----------\n');
+fprintf('Save output data to:\n%s\n',outputPath);
+fprintf('--------- Simulation Complete ----------\n');
 end
