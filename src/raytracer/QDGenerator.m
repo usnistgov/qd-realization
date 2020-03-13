8885225b50502887a97b69331dcf8aa09ceca899
Steve Blandino <steve.blandino@nist.gov>
2020-03-13 15:07:06 -0400

    Added dimension in output matrix to support multiple independent stochastic realizations

--- src/raytracer/QDGenerator.m
+++ src/raytracer/QDGenerator.m
@@ -1,6 +1,6 @@
 function [output, count1, switch_QD] = QDGenerator(order_of_R,output,...
     array_of_materials, number, MaterialLibrary, distance, freq, count1,...
-    dod, doa, vtx, v_temp, count, indexReference)
+    dod, doa, vtx, v_temp, count, indexReference, varargin)
 %INPUT -
 %order_of_R -  Order of reflection
 %output - multipath parameters
@@ -64,7 +64,14 @@ function [output, count1, switch_QD] = QDGenerator(order_of_R,output,...
 % States.
 %
 % Modified by: Mattia Lecci <leccimat@dei.unipd.it>, Improved access to MaterialLibrary
-
+var_struct = {'indStoc'}; 
+for k = 1:2:length(varargin)
+    if (~any(strcmp(varargin{k}, var_struct)))
+        warning(['Cannot specify "', varargin{k}, '" as input value - it will be discarted']);
+    end
+    eval([varargin{k},' = varargin{k+1};'])
+end
+if ~exist('indStoc','var'),     indStoc=1;        end
 
 c = 3e8;
 % if  switch_material==1 && QD_gen==1
@@ -79,15 +86,15 @@ for order_of_R_temp = 1:order_of_R
     
     Material = array_of_materials(number,order_of_R_temp);
     if order_of_R_temp == order_of_R
-        output(count1-1,21) = Material;
+        output(count1-1,21, 1:indStoc) = Material;
     end
     
     [Pathloss] = PathlossQD(MaterialLibrary,...
         array_of_materials(number,:),1);
     Pathloss1=Pathloss1+Pathloss;
 end
-output(count1-1,9) = output(count1-1,9)-(Pathloss1);
-New_pathgain = output(count1-1,9);
+output(count1-1,9, 1:indStoc) = output(count1-1,9,1:indStoc)-(Pathloss1);
+New_pathgain = output(count1-1,9,1:indStoc);
 
 % i1 is for generating precursors and i2 is for generating postcursors.
 for i1 = 1:2
@@ -122,33 +129,33 @@ for i1 = 1:2
     
     %%
     if  muk~=0
-        Kfactor = normalRandomGenerator(muk,sigmak);
-        gamma = normalRandomGenerator(muy,sigmay);
-        lambda1 = normalRandomGenerator(mul,sigmal);
-        sigma_Aod_E = abs(normalRandomGenerator(mus,sigmas));
-        sigma_Aod_A = abs(normalRandomGenerator(mus,sigmas));
-        sigma_Aoa_E = abs(normalRandomGenerator(mus,sigmas));
-        sigma_Aoa_A = abs(normalRandomGenerator(mus,sigmas));
+        Kfactor = normalRandomGenerator(muk,sigmak,indStoc);
+        gamma = normalRandomGenerator(muy,sigmay,indStoc);
+        lambda1 = normalRandomGenerator(mul,sigmal,indStoc);
+        sigma_Aod_E = abs(normalRandomGenerator(mus,sigmas,indStoc));
+        sigma_Aod_A = abs(normalRandomGenerator(mus,sigmas,indStoc));
+        sigma_Aoa_E = abs(normalRandomGenerator(mus,sigmas,indStoc));
+        sigma_Aoa_A = abs(normalRandomGenerator(mus,sigmas,indStoc));
         
-        lambda1 = abs(normalRandomGenerator(mul,sigmal));
-        tau_set = nan(n+1, 1);
-        tau_set(1) = (distance/c) * 1e9; % convert to ns
+        lambda1 = abs(normalRandomGenerator(mul,sigmal,indStoc));
+        tau_set = nan(n+1, indStoc);
+        tau_set(1,:) = (distance/c) * 1e9; % convert to ns
         
         i = 1;
         
         % generates delays
         while i<=n
             
-            diff = randomExponetialGenerator(lambda1);
+            diff = randomExponetialGenerator(lambda1).';
             if i1 == 1
-                tau_set(i+1) = tau_set(i)-diff;
+                tau_set(i+1,:) = tau_set(i,:)-diff;
             else
-                tau_set(i+1) = tau_set(i)+diff;
+                tau_set(i+1,:) = tau_set(i,:)+diff;
             end
-            output(count1+i-1,1) = count-1;
-            output(count1+i-1, 2:4) = dod;
-            output(count1+i-1, 5:7) = doa;
-            output(count1+i-1,8) = tau_set(i+1)*1e-9;
+            output(count1+i-1,1,1:indStoc) = count-1;
+            output(count1+i-1, 2:4,1:indStoc) = repmat(dod,1,1,indStoc);
+            output(count1+i-1, 5:7,1:indStoc) = repmat(doa,1,1,indStoc);
+            output(count1+i-1,8,1:indStoc) = tau_set(i+1,:)*1e-9;
             
             % output(count1,10)=180*atan(dod(2)/dod(1))/pi;
             % output(count1,11)=180*acos(dod(3)/norm(dod))/pi;
@@ -157,7 +164,7 @@ for i1 = 1:2
             i=i+1;
         end
         
-        PGcursor = New_pathgain;
+        PGcursor = squeeze(New_pathgain);
         
         % generates path loss
         
@@ -165,132 +172,135 @@ for i1 = 1:2
             
             if i~=0
                 if i1==1
-                    output(count1+i-1,9) = ((PGcursor)-...
-                        Kfactor+(((tau_set(i+1))-...
-                        (tau_set(1)))/gamma));
+                    output(count1+i-1,9,1:indStoc) = ((PGcursor)-...
+                        Kfactor+(((tau_set(i+1,:))-...
+                        (tau_set(1,:))).'./gamma));
                 else
-                    output(count1+i-1,9) = ((PGcursor)-...
-                        Kfactor-(((tau_set(i+1))-...
-                        (tau_set(1)))/gamma));
+                    output(count1+i-1,9,1:indStoc) = ((PGcursor)-...
+                        Kfactor-(((tau_set(i+1,:))-...
+                        (tau_set(1,:))).'./gamma));
                     if i == 1
-                        output(indexReference,9) = 20*log10(10^(PGcursor/10)-10^((PGcursor-Kfactor)/10));
+%                         output(indexReference,9,1:indStoc) = 20*log10(10.^(PGcursor/10)-10.^((PGcursor-Kfactor)/10));
                     end
                 end
             end
         end
         
         mu = acosd(dod(3)/norm(dod));
+        theta_cursor = repmat(mu,1,indStoc);
         
         % generates angular spread for Aod elevation
         
         for i=1:n
-            x = randomLaplaceGenerator();
-            ran(i) = x;
-            Aod_el(i) = mu+x;
-            theta_cursor = mu;
-            PG(i) = 10^(output(count1+i-1,9)/20);
-            PG1(i) = output(count1+i-1,9);
+            x = randomLaplaceGenerator(indStoc);
+            ran(i,:) = x;
+            Aod_el(i,:) = mu+x;
+            PG(i,:) = 10.^(output(count1+i-1,9,1:indStoc)/20);
+            PG1(i,:) = output(count1+i-1,9,1:indStoc);
             %     output(count1+i-1,11)=randomLaplaceGenerator(mu,sigma_Aod_E);
         end
-        PG_cursor = 10^(output(count-1,9)/20);
-        PG_cursor1 = output(count-1,9);
+        PG_cursor = squeeze(10.^(output(count-1,9,1:indStoc)/20)).';
+        PG_cursor1 = squeeze(output(count-1,9,1:indStoc)).';
         
-        mu_1 = (sum(PG.*Aod_el)+(PG_cursor*theta_cursor))...
-            /(sum(PG)+PG_cursor);
+        mu_1 = (sum(PG.*Aod_el)+(PG_cursor.*theta_cursor))...
+            ./(sum(PG)+PG_cursor);
         sigma_1 = sqrt(((sum(PG.*((Aod_el-mu_1).*...
-            (Aod_el-mu_1))))+(PG_cursor*...
-            ((theta_cursor-mu_1)^2)))/(sum(PG)+PG_cursor));
-        s = sigma_Aod_E/sigma_1;
-        Aod_el1 = [theta_cursor,Aod_el]';
-        x = (s^2)-((PG_cursor*((theta_cursor-mu_1)^2))/...
+            (Aod_el-mu_1))))+(PG_cursor.*...
+            ((theta_cursor-mu_1).^2)))./(sum(PG)+PG_cursor));
+        s = sigma_Aod_E.'./sigma_1;
+        Aod_el1 = [theta_cursor;Aod_el];
+        x = (s.^2)-((PG_cursor.*((theta_cursor-mu_1).^2))/...
             ((sum(PG.*((Aod_el-mu_1).*(Aod_el-mu_1))))+...
-            (PG_cursor*((theta_cursor-mu_1)^2))));
-        a=((sum(PG.*((Aod_el-mu_1).*(Aod_el-mu_1))))+(PG_cursor*((theta_cursor-mu_1)^2)));
-        b=PG_cursor*((theta_cursor-mu_1)^2);
-        s1=abs(x*(1+(b/a)));
+            (PG_cursor.*((theta_cursor-mu_1).^2))));
+        a=((sum(PG.*((Aod_el-mu_1).*(Aod_el-mu_1))))+(PG_cursor.*((theta_cursor-mu_1).^2)));
+        b=PG_cursor.*((theta_cursor-mu_1).^2);
+        s1=abs(x.*(1+(b./a)));
         Aod_el=((Aod_el-mu_1).*sqrt(s1))+(mu_1);
         
-        mu=output(count1-1,10);
-        
+        mu=squeeze(output(count1-1,10,1:indStoc));
+        theta_cursor=mu.';
         % generates angular spread for Aod azimuth
-        
+        ran = [];
         for i=1:n
-            x = randomLaplaceGenerator();
-            ran(i)=x;
-            Aod_az(i)=mu+x;
-            theta_cursor=mu;
+            x = randomLaplaceGenerator(indStoc);
+            ran(i,:)=x;
+            Aod_az(i,:)=mu+x;
+            
         end
         
-        mu_1=sum(PG.*Aod_az)/sum(PG);
-        sigma_1=sqrt((sum(PG.*((Aod_az-mu_1).*(Aod_az-mu_1))))/sum(PG));
-        s=sigma_Aod_A/sigma_1;
-        Aod_az1=[theta_cursor,Aod_az]';
-        x=(s^2)-((PG_cursor*((theta_cursor-mu_1)^2))/((sum(PG.*((Aod_az-mu_1).*(Aod_az-mu_1))))+(PG_cursor*((theta_cursor-mu_1)^2))));
-        a=((sum(PG.*((Aod_az-mu_1).*(Aod_az-mu_1))))+(PG_cursor*((theta_cursor-mu_1)^2)));
-        b=PG_cursor*((theta_cursor-mu_1)^2);
-        s1=abs(x*(1+(b/a)));
+        mu_1=sum(PG.*Aod_az)./sum(PG);
+        sigma_1=sqrt((sum(PG.*((Aod_az-mu_1).*(Aod_az-mu_1))))./sum(PG));
+        s=sigma_Aod_A.'./sigma_1;
+        Aod_az1=[theta_cursor;Aod_az];
+        x=(s.^2)-((PG_cursor.*((theta_cursor-mu_1).^2))./((sum(PG.*((Aod_az-mu_1).*(Aod_az-mu_1))))+(PG_cursor.*((theta_cursor-mu_1).^2))));
+        a=((sum(PG.*((Aod_az-mu_1).*(Aod_az-mu_1))))+(PG_cursor.*((theta_cursor-mu_1).^2)));
+        b=PG_cursor.*((theta_cursor-mu_1).^2);
+        s1=abs(x.*(1+(b./a)));
         Aod_az=((Aod_az-mu_1).*sqrt(s1))+(mu_1);
         
         
-        mu=output(count1-1,12);
-        
+        mu=squeeze(output(count1-1,12,1:indStoc));
+        theta_cursor=mu.';
         % generates angular spread for Aoa azimuth
         
+        ran = [];
         for i=1:n
-            x = randomLaplaceGenerator();
-            ran(i)=x;
-            Aoa_az(i)=mu+x;
-            theta_cursor=mu;
+            x = randomLaplaceGenerator(indStoc);
+            ran(i,:)=x;
+            Aoa_az(i,:)=mu+x;
+%             theta_cursor=mu;
         end
         
-        mu_1=sum(PG.*Aoa_az)/sum(PG);
-        sigma_1=sqrt((sum(PG.*((Aoa_az-mu_1).*(Aoa_az-mu_1))))/sum(PG));
-        s=sigma_Aoa_E/sigma_1;
-        Aoa_az1=[theta_cursor,Aoa_az]';
-        x=(s^2)-((PG_cursor*((theta_cursor-mu_1)^2))/((sum(PG.*((Aoa_az-mu_1).*(Aoa_az-mu_1))))+(PG_cursor*((theta_cursor-mu_1)^2))));
-        a=((sum(PG.*((Aoa_az-mu_1).*(Aoa_az-mu_1))))+(PG_cursor*((theta_cursor-mu_1)^2)));
-        b=PG_cursor*((theta_cursor-mu_1)^2);
-        s1=abs(x*(1+(b/a)));
+        mu_1=sum(PG.*Aoa_az)./sum(PG);
+        sigma_1=sqrt((sum(PG.*((Aoa_az-mu_1).*(Aoa_az-mu_1))))./sum(PG));
+        s=sigma_Aoa_E.'./sigma_1;
+        Aoa_az1=[theta_cursor;Aoa_az];
+        x=(s.^2)-((PG_cursor.*((theta_cursor-mu_1).^2))./((sum(PG.*((Aoa_az-mu_1).*(Aoa_az-mu_1))))+(PG_cursor.*((theta_cursor-mu_1).^2))));
+        a=((sum(PG.*((Aoa_az-mu_1).*(Aoa_az-mu_1))))+(PG_cursor.*((theta_cursor-mu_1).^2)));
+        b=PG_cursor.*((theta_cursor-mu_1).^2);
+        s1=abs(x.*(1+(b./a)));
         Aoa_az=((Aoa_az-mu_1).*sqrt(s1))+(mu_1);
         
         mu=acosd(doa(3)/norm(doa));
-        
+        theta_cursor = repmat(mu,1,indStoc);
+
+        ran = [];
         % generates angular spread for Aoa elevation
         for i=1:n
-            x = randomLaplaceGenerator();
-            ran(i)=x;
-            Aoa_el(i)=mu+x;
-            theta_cursor=mu;
+            x = randomLaplaceGenerator(indStoc);
+            ran(i,:)=x;
+            Aoa_el(i,:)=mu+x;
+%             theta_cursor=mu;
         end
         
-        mu_1=sum(PG.*Aoa_el)/sum(PG);
-        sigma_1=sqrt((sum(PG.*((Aoa_el-mu_1).*(Aoa_el-mu_1))))/sum(PG));
-        s=sigma_Aoa_E/sigma_1;
-        Aoa_el1=[theta_cursor,Aoa_el]';
-        x=(s^2)-((PG_cursor*((theta_cursor-mu_1)^2))/((sum(PG.*((Aoa_el-mu_1).*(Aoa_el-mu_1))))+(PG_cursor*((theta_cursor-mu_1)^2))));
-        a=((sum(PG.*((Aoa_el-mu_1).*(Aoa_el-mu_1))))+(PG_cursor*((theta_cursor-mu_1)^2)));
-        b=PG_cursor*((theta_cursor-mu_1)^2);
-        s1=abs(x*(1+(b/a)));
+        mu_1=sum(PG.*Aoa_el)./sum(PG);
+        sigma_1=sqrt((sum(PG.*((Aoa_el-mu_1).*(Aoa_el-mu_1))))./sum(PG));
+        s=sigma_Aoa_E.'./sigma_1;
+        Aoa_el1=[theta_cursor;Aoa_el];
+        x=(s.^2)-((PG_cursor.*((theta_cursor-mu_1).^2))/((sum(PG.*((Aoa_el-mu_1).*(Aoa_el-mu_1))))+(PG_cursor.*((theta_cursor-mu_1).^2))));
+        a=((sum(PG.*((Aoa_el-mu_1).*(Aoa_el-mu_1))))+(PG_cursor.*((theta_cursor-mu_1).^2)));
+        b=PG_cursor.*((theta_cursor-mu_1).^2);
+        s1=abs(x.*(1+(b./a)));
         Aoa_el=((Aoa_el-mu_1).*sqrt(s1))+(mu_1);
         
         % sorting all QD parameters in 'QD' parametwr
         for i=1:n
-            output(count1+i-1, 11)=Aod_el1(i+1);
-            output(count1+i-1, 10)=Aod_az1(i+1);
-            output(count1+i-1, 12)=Aoa_az1(i+1);
-            output(count1+i-1, 13)=Aoa_el1(i+1);
-            output(count1+i-1, 14:18)=output(count1-1, 14:18);
+            output(count1+i-1, 11, 1:indStoc)=Aod_el1(i+1,:);
+            output(count1+i-1, 10, 1:indStoc)=Aod_az1(i+1,:);
+            output(count1+i-1, 12, 1:indStoc)=Aoa_az1(i+1,:);
+            output(count1+i-1, 13, 1:indStoc)=Aoa_el1(i+1,:);
+            output(count1+i-1, 14:18, 1:indStoc)=output(count1-1, 14:18, 1:indStoc);
             
-            vAngle_DoD = Aod_el1(i+1);
-            hAngle_DoD = Aod_az1(i+1);
-            dod_temp=[sind(vAngle_DoD)*cosd(hAngle_DoD),sind(vAngle_DoD)*sind(hAngle_DoD),cosd(vAngle_DoD)];
-            vtx_along_dod = dot(vtx, -dod_temp);
-            vrx_along_dod = dot(v_temp, -dod_temp);
+            vAngle_DoD = Aod_el1(i+1,:);
+            hAngle_DoD = Aod_az1(i+1,:);
+            dod_temp=reshape([sind(vAngle_DoD).*cosd(hAngle_DoD),sind(vAngle_DoD).*sind(hAngle_DoD),cosd(vAngle_DoD)], [], 3).';
+            vtx_along_dod = dot(repmat(vtx, indStoc,1).', -dod_temp);
+            vrx_along_dod = dot(repmat(v_temp, indStoc,1).', -dod_temp);
             c = 3e8;
             doppler_factor = freq * (vrx_along_dod - vtx_along_dod) / c;
-            output(count1+i-1,20) = doppler_factor;
-            output(count1+i-1,18) = rand*2*pi;
-            output(count1+i-1,19) = output(count1+i-1,9)*(output(count1-1,19)/output(count1-1,9));
+            output(count1+i-1,20,1:indStoc) = doppler_factor;
+            output(count1+i-1,18,1:indStoc) = rand*2*pi;
+            output(count1+i-1,19,1:indStoc) = output(count1+i-1,9,1:indStoc).*(output(count1-1,19,1:indStoc)./output(count1-1,9,1:indStoc));
             switch_QD = 1;
         end
         
