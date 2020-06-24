9351efed9b7286c78348b2a55e72304925f7033f
Steve Blandino <steve.blandino@nist.gov>
2020-06-24 12:22:15 -0400

    Removed conversion inch to m

--- src/utils/savePositionFromTrace.m
+++ src/utils/savePositionFromTrace.m
@@ -9,7 +9,7 @@ traceField = fieldnames(trace);
 traceSelect = ~cellfun(@isempty,(regexp(traceField,['.', captureId])));
 if sum(traceSelect)
     trace = eval(['trace.',traceField{traceSelect}]);
-    position = trace(:,[1 3 2])*2.54/100;
+    position = trace(:,[1 3 2]);
     writematrix(position, nameFile)
 else
     warning('Trace not found')
