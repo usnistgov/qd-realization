db9b126421c67f1698453707ae052aae1ee5f9b4
Varshney <nnv2@NIST.GOV>
2020-07-08 12:53:25 -0400

    Modifications

--- src/raytracer/xmlreader.m
+++ src/raytracer/xmlreader.m
@@ -1,8 +1,8 @@
-function [CADOutput,  materialSwitch] = xmlreader(filename, ...
-    MaterialLibrary,  referencePoint,  r,  IndoorSwitch)
+function [CADOutput, materialSwitch] = xmlreader(filename, ...
+    MaterialLibrary, referencePoint, r, IndoorSwitch)
 %XML redaer extracts the information of CAD file (AMF). The input of the
-%function is filename,  the material database with all the material
-%parameters (Material_library),  reference point (referencePoint) and distance limitation(r)
+%function is filename, the material database with all the material
+%parameters (Material_library), reference point (referencePoint) and distance limitation(r)
 %The output is extracted triangles (CADop), 
 %and a boolean to know whether the material information is
 %present in the CAD file (switch1)
@@ -10,39 +10,39 @@ function [CADOutput,  materialSwitch] = xmlreader(filename, ...
 
 % -------------Software Disclaimer---------------
 %
-% NIST-developed software is provided by NIST as a public service. You may use,  copy
-% and distribute copies of the software in any medium,  provided that you keep intact this
-% entire notice. You may improve,  modify and create derivative works of the software or
-% any portion of the software,  and you may copy and distribute such modifications or
+% NIST-developed software is provided by NIST as a public service. You may use, copy
+% and distribute copies of the software in any medium, provided that you keep intact this
+% entire notice. You may improve, modify and create derivative works of the software or
+% any portion of the software, and you may copy and distribute such modifications or
 % works. Modified works should carry a notice stating that you changed the software
 % and should note the date and nature of any such change. Please explicitly
 % acknowledge the National Institute of Standards and Technology as the source of the
 % software.
 %
 % NIST-developed software is expressly provided "AS IS." NIST MAKES NO
-% WARRANTY OF ANY KIND,  EXPRESS,  IMPLIED,  IN FACT OR ARISING BY
-% OPERATION OF LAW,  INCLUDING,  WITHOUT LIMITATION,  THE IMPLIED
-% WARRANTY OF MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE, 
+% WARRANTY OF ANY KIND, EXPRESS, IMPLIED, IN FACT OR ARISING BY
+% OPERATION OF LAW, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+% WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, 
 % NON-INFRINGEMENT AND DATA ACCURACY. NIST NEITHER REPRESENTS
 % NOR WARRANTS THAT THE OPERATION OF THE SOFTWARE WILL BE
-% UNINTERRUPTED OR ERROR-FREE,  OR THAT ANY DEFECTS WILL BE
+% UNINTERRUPTED OR ERROR-FREE, OR THAT ANY DEFECTS WILL BE
 % CORRECTED. NIST DOES NOT WARRANT OR MAKE ANY REPRESENTATIONS
 % REGARDING THE USE OF THE SOFTWARE OR THE RESULTS THEREOF, 
-% INCLUDING BUT NOT LIMITED TO THE CORRECTNESS,  ACCURACY, 
-% RELIABILITY,  OR USEFULNESS OF THE SOFTWARE.
+% INCLUDING BUT NOT LIMITED TO THE CORRECTNESS, ACCURACY, 
+% RELIABILITY, OR USEFULNESS OF THE SOFTWARE.
 %
 % You are solely responsible for determining the appropriateness of using
-% anddistributing the software and you assume all risks associated with its use,  including
-% but not limited to the risks and costs of program errors,  compliance with applicable
-% laws,  damage to or loss of data,  programs or equipment,  and the unavailability or
+% anddistributing the software and you assume all risks associated with its use, including
+% but not limited to the risks and costs of program errors, compliance with applicable
+% laws, damage to or loss of data, programs or equipment, and the unavailability or
 % interruption of operation. This software is not intended to be used in any situation
 % where a failure could cause risk of injury or damage to property. The software
 % developed by NIST employees is not subject to copyright protection within the United
 % States.
 %
-% Modified by: Mattia Lecci <leccimat@dei.unipd.it>,  Used MATLAB functions instead of custom ones, 
-%    improved MaterialLibrary access,  readibility,  performance in general
-% Modified by: Neeraj Varshney <neeraj.varshney@nist.gov>,  support multiple
+% Modified by: Mattia Lecci <leccimat@dei.unipd.it>, Used MATLAB functions instead of custom ones, 
+%    improved MaterialLibrary access, readibility, performance in general
+% Modified by: Neeraj Varshney <neeraj.varshney@nist.gov>, support multiple
 % objects and different length units in amf file
 
 
@@ -164,7 +164,7 @@ for iterateObjects = 1:lengthObject                            % For multiple ob
 
                 % Storing triangle vertices and plane equations in output
                 % Part where output file is created. It contains the triangle vertices
-                % in first nine columns,  plane equations in the next four columns
+                % in first nine columns, plane equations in the next four columns
                 if switch2==0
                     materialSwitch=0;
                 end
