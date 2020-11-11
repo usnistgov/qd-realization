edbf97f6e148a8c8c1c59053f68ff89106a6e869
Steve Blandino <steve.blandino@nist.gov>
2020-11-11 16:24:35 -0500

    Delete functions not used

--- src/utils/jsonFixRemove.m
+++
@@ -1,6 +0,0 @@
-function jsonString = jsonFixRemove(jsonString, str2Remove)
-rem_ind_start = num2cell(strfind(jsonString, str2Remove)); % Find start string to remove
-index2rm = cell2mat(cellfun(@(x) x:x+length(str2Remove)-1,rem_ind_start,'UniformOutput',false)); % Create index of char to remove
-jsonString(index2rm) = []; % Remove temporary vector
-
-end
--- src/utils/jsonFixReplace.m
+++
@@ -1,8 +0,0 @@
-function jsonString = jsonFixReplace(jsonString, str2Remove, str2Replace)
-rem_ind_start = num2cell(strfind(jsonString, str2Remove)); % Find start string to remove
-index2rm = cell2mat(cellfun(@(x) x:x+length(str2Remove)-1,rem_ind_start,'UniformOutput',false)); % Create index of char to remove
-jsonString(index2rm) = []; % Remove temporary vector
-if ~isempty(rem_ind_start)
-    jsonString = [jsonString(1:rem_ind_start{1}-1), str2Replace, jsonString(rem_ind_start{1}:end)];
-end
-end
