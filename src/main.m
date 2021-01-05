928b2c45e3dcbaf3669af04636b973024ff57059
Neeraj Varshney <neeraj.varshney@nist.gov>
2021-01-05 03:55:26 -0500

    Update main.m

--- src/main.m
+++ src/main.m
@@ -58,7 +58,7 @@ end
 % To Run Existing Scenario Given in 'src/examples/' Folder, 
 % Set scenarioNameStr = 'examples/FolderName' 
 % e.g., scenarioNameStr = 'examples/Indoor1';
-scenarioNameStr = 'examples/StreetCanyon';
+scenarioNameStr = '';
 
 if ~isempty(scenarioNameStr)
     fprintf('Use customized scenario: %s.\n',scenarioNameStr);
