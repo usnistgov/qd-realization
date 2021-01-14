faa385b68aaf900937f6434e133823227da5b4f1
Neeraj Varshney <neeraj.varshney@nist.gov>
2021-01-14 05:04:22 -0500

    L-Room.xml update

--- src/main.m
+++ src/main.m
@@ -58,7 +58,7 @@ end
 % To Run Existing Scenario Given in 'src/examples/' Folder, 
 % Set scenarioNameStr = 'examples/FolderName' 
 % e.g., scenarioNameStr = 'examples/Indoor1';
-scenarioNameStr = 'examples/Indoor1';
+scenarioNameStr = 'examples/LivingRoom';
 
 if ~isempty(scenarioNameStr)
     fprintf('Use customized scenario: %s.\n',scenarioNameStr);
